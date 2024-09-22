0a1b73f0055aceb05bc4c87146b88f6ce0af436c
FEP3C <pccctech2023@163.com>
2024-09-22 13:48:24 +0800

    Fixed the database...

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use actix_web::{get, App, HttpServer, Responder};
+use sqlx::mysql::MySqlPoolOptions;
+use std::env;
+use dotenv::dotenv;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -7,9 +10,23 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting server at http://127.0.0.1:8080");
-    HttpServer::new(|| {
+    dotenv().ok(); // 加载.env文件中的环境变量
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set in .env");
+
+    // 创建数据库连接池
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to create pool.");
+
+    println!("Connected to the database!");
+
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone()) // 将数据库连接池添加到应用状态
             .service(hello)
     })
     .bind("127.0.0.1:8080")?
